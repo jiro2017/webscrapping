80ffb6cfc92fd7a05bda877d58cdaf16ef617962
Ujiro O <oduokpeujiro@outlook.com>
2023-10-25 10:42:36 +0100

    working on index

--- index.php
+++ index.php
@@ -11,6 +11,10 @@ $headers = array(
 curl_setopt($handler, CURLOPT_HEADER, 0);
 curl_setopt($handler, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($handler, CURLOPT_PROXY, "102.68.128.214"); 
+// curl_setopt($handler, CURLOPT_PROXYPORT, "8080"); 
+// curl_setopt($handler, CURLOPT_PROXY, CURLPROXY_HTTP);
+
 $result = curl_exec($handler);
 curl_close($handler);
 
