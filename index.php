718633f030b2c9517ec057df7d05a0d8b2391808
Ujiro O <oduokpeujiro@outlook.com>
2023-10-25 11:30:22 +0100

    working on index

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 
 // echo $_SERVER['HTTP_USER_AGENT'];
 // return;
-$url = "https://www.jiropages.com";
+$url = "https://www.facebook.com";
 $handler = curl_init($url);
 $headers = array(
-    "Content-Language: en",
+    // "Content-Language: en",
+    "Connection : keep-alive",
+    'Accept: */*',
+    "Accept-Language : en-us",
     "User-Agent : Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/118.0"
 );
 curl_setopt($handler, CURLOPT_HEADER, 0);
