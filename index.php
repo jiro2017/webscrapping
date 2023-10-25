a0a1869e7bd50f461970a4fddc7c3ab0e1f0e0f6
Ujiro O <oduokpeujiro@outlook.com>
2023-10-25 10:16:04 +0100

    working on index

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 
 // echo $_SERVER['HTTP_USER_AGENT'];
-return;
+// return;
 $url = "https://www.facebook.com";
 $handler = curl_init($url);
 $headers = array(
     "Content-Language: en",
     "User-Agent : Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/118.0"
 );
+curl_setopt($handler, CURLOPT_HEADER, 0);
 curl_setopt($handler, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($handler);
+curl_close($handler);
+
+echo $result;
