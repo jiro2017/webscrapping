819f3ab3648083c6b3d78fb203367560a38bcaed
Ujiro O <oduokpeujiro@outlook.com>
2023-10-26 11:47:34 +0100

    working on index

--- index.php
+++ index.php
@@ -1,25 +1,33 @@
 <?php
+require_once 'vendor/autoload.php';
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+$serverUrl = 'http://localhost:4444';
+// Chrome
+$driver = RemoteWebDriver::create($serverUrl, DesiredCapabilities::chrome());
+// $driver = ChromeDriver::start();
+echo $driver->get('https://google.com');
 // echo $_SERVER['HTTP_USER_AGENT'];
 // return;
-$url = "test.php"; //"https://www.facebook.com";
-$handler = curl_init($url);
-$headers = array(
-    // "Content-Language: en",
-    "Connection : keep-alive",
-    "Accept: */*",
-    "Accept-Language : en-us",
-    "User-Agent : Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/118.0"
-);
-curl_setopt($handler, CURLOPT_COOKIEJAR, dirname(__FILE__).'/cookie.txt');
-curl_setopt($handler, CURLOPT_HEADER, 0);
-curl_setopt($handler, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($handler, CURLINFO_HEADER_OUT, true);
-// curl_setopt($handler, CURLOPT_PROXY, "102.68.128.214"); 
-// curl_setopt($handler, CURLOPT_PROXYPORT, "8080"); 
-// curl_setopt($handler, CURLOPT_PROXY, CURLPROXY_HTTP);
+// $url = "test.php"; //"https://www.facebook.com";
+// $handler = curl_init($url);
+// $headers = array(
+//     // "Content-Language: en",
+//     "Connection : keep-alive",
+//     "Accept: */*",
+//     "Accept-Language : en-us",
+//     "User-Agent : Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/118.0"
+// );
+// curl_setopt($handler, CURLOPT_COOKIEJAR, dirname(__FILE__).'/cookie.txt');
+// curl_setopt($handler, CURLOPT_HEADER, 0);
+// curl_setopt($handler, CURLOPT_HTTPHEADER, $headers);
+// curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+// // curl_setopt($handler, CURLINFO_HEADER_OUT, true);
+// // curl_setopt($handler, CURLOPT_PROXY, "102.68.128.214"); 
+// // curl_setopt($handler, CURLOPT_PROXYPORT, "8080"); 
+// // curl_setopt($handler, CURLOPT_PROXY, CURLPROXY_HTTP);
 
-$result = curl_exec($handler);
-curl_close($handler);
-// echo json_encode(curl_getinfo($handler)); return;
-echo $result;
+// $result = curl_exec($handler);
+// curl_close($handler);
+// // echo json_encode(curl_getinfo($handler)); return;
+// echo $result;
