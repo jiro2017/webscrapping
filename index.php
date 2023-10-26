82d1988b1d397e461c14de2a566c32110d5289af
Ujiro O <oduokpeujiro@outlook.com>
2023-10-26 21:13:04 +0100

    working on index

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 require_once 'vendor/autoload.php';
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
-$serverUrl = 'http://localhost:9515';
+$serverUrl = 'http://localhost:9515'; //'http://192.168.0.117:4444';
 // Chrome
 $driver = RemoteWebDriver::create($serverUrl, DesiredCapabilities::chrome());
-$driver->get("https://www.google.com");
+//get the desired webpage
+$driver->get("https://www.konga.com/search?search=gas+cookers");
+
+//quit the browsing session
+// $driver->quit();
