8b3b68aed5edefb3a62e2aab14ef81a7a8fdd8d1
Ujiro O <oduokpeujiro@outlook.com>
2023-10-27 12:34:29 +0100

    working on guzzle/http

--- index.php
+++ index.php
@@ -15,11 +15,18 @@ require_once 'vendor/autoload.php';
 /** Scrapping webpages using Guzzle **/
 $httpClient = new \GuzzleHttp\Client();
 $response = $httpClient->get('https://www.amazon.com/s?k=gas+stove&crid=3S0IM7D9RAPY1&sprefix=gas+stov%2Caps%2C384&ref=nb_sb_noss_2');
-// echo $response->getBody(); return;
+echo $response->getBody(); //return;
 $htmlString = (string) $response->getBody();
-echo $htmlString; return;
+// echo $htmlString; return;
 //add this line to suppress any warnings
 libxml_use_internal_errors(true);
 $doc = new DOMDocument();
 $doc->loadHTML($htmlString);
 $xpath = new DOMXPath($doc);
+
+$products = $xpath->evaluate('//div[@class="puisg-row"]');
+var_dump($products); return;
+foreach($products as $product) {
+    $product_name = $product->evaluate("//div[@class='a-section']//div[@class='puisg-row']//div[@class='puis-list-col-right]//div//div//div//h2//a//span']")->textContent.PHP_EOL;
+    echo $product_name;
+}
